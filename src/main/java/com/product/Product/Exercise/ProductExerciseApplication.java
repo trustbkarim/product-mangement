dcb291b0adccf4b55c655b4b6986d59f6cdb9719
Karim BOUARFA <trustbkarim@gmail.com>
2024-12-28 20:22:36 +0100

    first commit

---
+++ src/main/java/com/product/Product/Exercise/ProductExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.product.Product.Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/Product/Exercise/ProductExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.Product.Exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
